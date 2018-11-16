6e1c00ec3d0ab13f362a054d7532544480b7f3ae
Laura R <laurarachel@gmail.com>
2018-11-16 11:04:58 -0800

    refactor directory organization

---
+++ MadHatter/src/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, Text, View} from 'react-native';
+import styles from '../public/Stylesheet'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to Mad Hatter!</Text>
+        <Text style={styles.instructions}>Wear all your hats!</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
